66f010187c03764688595c372c968d19271e6d4b
Kirill Klem <klemkirill20@gmail.com>
2023-03-29 19:19:22 +0300

    add test_player

--- main/players/views.py
+++ main/players/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from .models import Player
+from main.extensions import db
 
 blueprint = Blueprint('players', __name__)
 
@@ -7,3 +8,20 @@ blueprint = Blueprint('players', __name__)
 def save_player():
     print(request.json)
     return jsonify({'status': 'created'}), 201
+
+
+@blueprint.route('/api/player/<position>', methods=['POST'])
+def test_player(position):
+    models = {
+        mapper.class_.__mapper_args__["polymorphic_identity"]:mapper.class_
+        for mapper in db.Model.registry.mappers
+    }
+    position = position.upper()
+    if position in models:
+        player = models[position](**{
+            'name':'test',
+            'age':24,
+            'matches':10,
+        })
+    db.session.add(player)
+    db.session.commit()
