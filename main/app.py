1cc7fcd8af3bc10f47cb48e0a8ae08662adc86ba
Kirill Klem <klemkirill20@gmail.com>
2023-04-06 21:25:28 +0300

    Add click commands

--- main/app.py
+++ main/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from main.extensions import db, migrate
-from main import players
+import players
+
+
 
 
 def create_app(config_object="main.settings"):
@@ -8,6 +10,7 @@ def create_app(config_object="main.settings"):
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
+    register_commands(app)
 
     return app
 
@@ -20,3 +23,6 @@ def register_extensions(app):
 def register_blueprints(app):
     app.register_blueprint(players.views.blueprint)
     return None
+
+def register_commands(app):
+    app.cli.add_command(players.commands.print_gk)
