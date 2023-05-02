cb5e5d7ba897bace7b6ad8a4708e55b293022b3e
Kirill Klem <klemkirill20@gmail.com>
2023-05-02 22:32:32 +0300

    add constants for counting score

---
+++ players/constants.py
@@ -0,0 +1,8 @@
+DIRECT_TEAM_INFLUENCE_GK = ['team_success_on_minus_off', 'team_success_ong', 'team_success_onga',
+'team_success_plus_in_minus', 'team_success_plus_in_minus_90', 'team_success_ppm',
+'team_success_xg_on_minus_off', 'team_success_xg_onxg', 'team_success_xg_onxga',
+'team_success_xg_xg_plus_in_minus', 'team_success_xg_xg_plus_in_minus_90']
+
+DEFENSIVE_TEAM_INFLUENCE_GK = ['touches_def_3rd', 'touches_def_pen']
+
+TEAM_INFLUENCE_GK = ['DIRECT_TEAM_INFLUENCE_GK', 'DEFENSIVE_TEAM_INFLUENCE_GK']
