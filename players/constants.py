2b6553138d6d39acf068bdf7d3b6b969142681bb
Kirill Klem <klemkirill20@gmail.com>
2023-05-04 20:07:26 +0300

    change list on dict for weighted average

--- players/constants.py
+++ players/constants.py
@@ -1,8 +1,23 @@
-DIRECT_TEAM_INFLUENCE_GK = ['team_success_on_minus_off', 'team_success_ong', 'team_success_onga',
-'team_success_plus_in_minus', 'team_success_plus_in_minus_90', 'team_success_ppm',
-'team_success_xg_on_minus_off', 'team_success_xg_onxg', 'team_success_xg_onxga',
-'team_success_xg_xg_plus_in_minus', 'team_success_xg_xg_plus_in_minus_90']
+DIRECT_TEAM_INFLUENCE_GK = {
+    'team_success_on_minus_off': 10,
+    'team_success_ong': 10,
+    'team_success_onga': 10,
+    'team_success_plus_in_minus': 0,
+    'team_success_plus_in_minus_90': 50,
+    'team_success_ppm': 30,
+    'team_success_xg_on_minus_off': 0,
+    'team_success_xg_onxg': 20,
+    'team_success_xg_onxga': 20,
+    'team_success_xg_xg_plus_in_minus': 0,
+    'team_success_xg_xg_plus_in_minus_90': 50,
+}
 
-DEFENSIVE_TEAM_INFLUENCE_GK = ['touches_def_3rd', 'touches_def_pen']
+DEFENSIVE_TEAM_INFLUENCE_GK = {
+    'touches_def_3rd': 50, 
+    'touches_def_pen': 50,
+}
 
-TEAM_INFLUENCE_GK = ['DIRECT_TEAM_INFLUENCE_GK', 'DEFENSIVE_TEAM_INFLUENCE_GK']
+TEAM_INFLUENCE_GK = {
+    'DIRECT_TEAM_INFLUENCE_GK': 45, 
+    'DEFENSIVE_TEAM_INFLUENCE_GK': 65,
+    }
