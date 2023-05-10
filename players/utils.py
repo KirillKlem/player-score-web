47906d73dfd79a12b255c874c8c42effaa0d8d88
Kirill Klem <klemkirill20@gmail.com>
2023-05-10 20:14:31 +0300

    add docstring and type annotation

--- players/utils.py
+++ players/utils.py
@@ -1,5 +1,9 @@
 from sqlalchemy.orm import ColumnProperty, class_mapper
 
-def attribute_names(cls):
+def attribute_names(cls) -> list[str]:
+    '''
+    Get all the attributes of a class (cls), accounting polymorphism
+    '''
+    
     properties = class_mapper(cls)._polymorphic_properties
     return [prop.key for prop in properties] 
