bb2fc07516d0d25343ebee398e4e4a42ad61c84a
Kirill Klem <klemkirill20@gmail.com>
2023-05-11 20:48:34 +0300

    add constants for passing, basic goalkeepering and their accompanying

--- players/constants.py
+++ players/constants.py
@@ -13,15 +13,87 @@ DIRECT_TEAM_INFLUENCE_GK = {
 }
 
 DEFENSIVE_TEAM_INFLUENCE_GK = {
-    'touches_def_3rd': 50, 
-    'touches_def_pen': 50,
+    'touches_def_3rd': 30, 
+    'touches_def_pen': 30,
+    'total_att': 60, 
 }
 
 TEAM_INFLUENCE_GK = {
     'DIRECT_TEAM_INFLUENCE_GK': 45, 
     'DEFENSIVE_TEAM_INFLUENCE_GK': 65,
-    }
+}
+
+PASSING_OVERALL_GK = {
+    'total_cmp': 20,
+    'total_cmp_percent': 80,
+    'total_prgdist': 65,
+    'total_totdist': 55,
+}
+
+PASSING_SHORT_GK = {
+    'short_att': 35,
+    'short_cmp': 45,
+    'short_cmp_percent': 70,
+}
+
+PASSING_MEDIUM_GK = {
+    'medium_att': 40,
+    'medium_cmp': 50,
+    'medium_cmp_percent': 70,
+}
+
+PASSING_LONG_GK = {
+    'long_att': 20,
+    'long_cmp': 40,
+    'long_cmp_percent': 90,
+}
+
+PASSING_ATACKING_GK = {
+    'ast': 5,
+    'xa': 5,
+    'xag': 5,
+    'kp': 30,
+    'on1_in_3': 40,
+    'ppa': 20,
+    'prgp': 50,
+}
+
+PASSING_GK = {
+    'PASSING_OVERALL_GK': 33,
+    'PASSING_SHORT_GK': 50,
+    'PASSING_MEDIUM_GK': 40,
+    'PASSING_LONG_GK': 28,
+    'PASSING_ATACKING_GK': 17,
+}
+
+ALLOWED_GOALKEEPING_GK = {
+    'performance_ga': 50,
+    'performance_ga90': 50,
+}
+
+SAVES_GOALKEEPING_GK = {
+    'performance_save_percent': 95,
+    'performance_saves': 90,
+    'performance_cs': 30,
+    'performance_cs_percent': 35,
+}
+
+PENALTY_SAVES_GOALKEEPING_GK = {
+    'penalty_kicks_pksv': 50,
+    'penalty_kicks_save_percent': 75,
+    'penalty_kicks_pka': 10,
+}
+
+GOALKEEPING_GK = {
+    'ALLOWED_GOALKEEPING_GK': 40,
+    'SAVES_GOALKEEPING_GK': 95,
+    'PENALTY_SAVES_GOALKEEPING_GK': 10,
+}
 
 TOTAL_SCORE_GK = {
-    'TEAM_INFLUENCE_GK': 100,
+    'TEAM_INFLUENCE_GK': 10,
+    'PASSING_GK': 40,
+    'GOALKEEPING_GK': 95,
 }
+
+NEGATIVE_PLAYER_STATS = ['performance_ga', 'performance_ga90', 'penalty_kicks_pka']
