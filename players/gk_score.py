851a4a98c0a22186b73d0f9b41fb4ce872aaa51e
Kirill Klem <klemkirill20@gmail.com>
2023-05-16 21:18:22 +0300

    add updating db, rename variables, clear of unused

--- players/gk_score.py
+++ players/gk_score.py
@@ -1,8 +1,7 @@
-from .count_score import get_names, get_all_stats, count_main_weighted_average, \
-    add_to_all_score, add_to_final_score, count_total_score, create_weighted_average
+from .count_score import create_models, get_names, get_all_stats, count_main_weighted_average, \
+    add_to_all_score, add_to_final_score, count_total_score, create_weighted_average, update_db
 from . import constants
 
-from pprint import pprint
 
 def count_gk_score():
     '''
@@ -12,15 +11,16 @@ def count_gk_score():
     ...
     '''
 
-    gk_names = get_names('GK')
-    goalkeeper_stats = get_all_stats(gk_names)
-    all_gk_score = {player: {} for player in gk_names}
-    final_gk_score = {player: {} for player in gk_names}
+    gk_names = get_names(position='GK', condition=600)
+    gk_models = create_models(gk_names)
+    gk_stats = get_all_stats(gk_names)
+    all_gk_score = {gk: {} for gk in gk_names}
+    final_gk_score = {gk: {} for gk in gk_names}
 
     def count_group_stats(main_constant):
         weighted_average = {}
         for constant in getattr(constants, main_constant):
-            weighted_average[constant.lower()] = create_weighted_average(gk_names, goalkeeper_stats, getattr(constants, constant))
+            weighted_average[constant.lower()] = create_weighted_average(gk_names, gk_stats, getattr(constants, constant))
             add_to_all_score(weighted_average, all_gk_score)
 
         result_stats_gk = count_main_weighted_average(gk_names, weighted_average, getattr(constants, main_constant))
@@ -30,3 +30,4 @@ def count_gk_score():
         count_group_stats(const)
 
     count_total_score(final_gk_score, getattr(constants, 'TOTAL_SCORE_GK'))
+    update_db(player_models=gk_models, player_score=final_gk_score)
