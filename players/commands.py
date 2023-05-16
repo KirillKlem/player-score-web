5453f903ed54937458f377943e13f5de636da51a
Kirill Klem <klemkirill20@gmail.com>
2023-05-16 21:15:35 +0300

    add def score

--- players/commands.py
+++ players/commands.py
@@ -1,10 +1,11 @@
 from main.extensions import db
 from .models import Player
 from .gk_score import count_gk_score
+from .def_score import count_def_score
 
 import click
 
 @click.command()
-def print_gk():
+def count_score():
     count_gk_score()
-    
+    count_def_score()
