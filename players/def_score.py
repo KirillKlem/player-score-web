3eeadc0a12b724f8d8d84f1b9353c5c1832918e5
Kirill Klem <klemkirill20@gmail.com>
2023-05-18 22:03:23 +0300

    enable main functionality

--- players/def_score.py
+++ players/def_score.py
@@ -11,20 +11,21 @@ def count_def_score():
     all_df_score = {df: {} for df in df_names}
     final_df_score = {df: {} for df in df_names}
 
-    # Note:
-    # Основная часть не работает пока не добавлены константы!!!
+    # pprint(df_stats)
 
-    # def count_group_stats(main_constant):
-    #     weighted_average = {}
-    #     for constant in getattr(constants, main_constant):
-    #         weighted_average[constant.lower()] = create_weighted_average(df_names, df_stats, getattr(constants, constant))
-    #         add_to_all_score(weighted_average, all_df_score)
+    def count_group_stats(main_constant):
+        weighted_average = {}
+        for constant in getattr(constants, main_constant):
+            weighted_average[constant.lower()] = create_weighted_average(df_names, df_stats, getattr(constants, constant))
+            add_to_all_score(weighted_average, all_df_score)
 
-    #     result_stats_gk = count_main_weighted_average(df_names, weighted_average, getattr(constants, main_constant))
-    #     add_to_final_score(main_constant.lower(), result_stats_gk, final_df_score)
+        result_stats_gk = count_main_weighted_average(df_names, weighted_average, getattr(constants, main_constant))
+        add_to_final_score(main_constant.lower(), result_stats_gk, final_df_score)
             
-    # for const in constants.TOTAL_SCORE_DF:
-    #     count_group_stats(const)
+    for const in constants.TOTAL_SCORE_DF:
+        count_group_stats(const)
 
-    # count_total_score(final_df_score, getattr(constants, 'TOTAL_SCORE_GK'))
+    count_total_score(final_df_score, getattr(constants, 'TOTAL_SCORE_DF'))
+    pprint(all_df_score)
+    pprint(final_df_score)
     # update_db(player_models=df_models, player_score=final_df_score)
